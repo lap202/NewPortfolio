bb265a0073ba109e5acda158654296f42820b35d
Andrew Bach <62669638+lap202@users.noreply.github.com>
2022-09-24 02:28:55 -0500

    Upload initial Progress

---
+++ New Portfolio/js/imageswap.js
@@ -0,0 +1,27 @@
+//Code for onHover image swaps for the github and linkedin icons
+
+"use strict";
+function github_onHover()
+{
+    $("#githubLink").attr('src', 'img\\githublogoOnHover.png');
+}
+function github_offHover()
+{
+    $("#githubLink").attr('src', 'img\\githublogo.png');
+}
+function linkedin_onHover()
+{
+    $("#linkedinLink").attr('src', 'img\\linkedinlogoOnHover.png');
+}
+function linkedin_offHover()
+{
+    $("#linkedinLink").attr('src', 'img\\linkedinlogo.png');
+}
+function resume_onHover()
+{
+    $("#resumeLink").attr('src', 'img\\resumelogoOnHover.png');
+}
+function resume_offHover()
+{
+    $("#resumeLink").attr('src', 'img\\resumelogo.png');
+}
